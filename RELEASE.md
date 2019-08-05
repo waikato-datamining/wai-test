d71e219e91bc8a7e66416b3fa536c52155c516f5
csterling <coreytsterling@gmail.com>
2019-08-05 16:12:43 +1200

    Added initial dist files.

---
+++ RELEASE.md
@@ -0,0 +1,28 @@
+Pypi
+====
+
+Preparation:
+* increment version in `setup.py`
+* add new changelog section in `CHANGES.rst`
+* commit/push all changes
+
+Commands for releasing on pypi.org (requires twine >= 1.8.0):
+
+```
+  find -name "*~" -delete
+  rm dist/*
+  python3 setup.py clean
+  python3 setup.py sdist
+  twine upload dist/*
+```
+
+
+Github
+======
+
+Steps:
+* start new release (version: `vX.Y.Z`)
+* enter release notes, i.e., significant changes since last release
+* upload `wai.test-X.Y.Z.tar.gz` previously generated with `setup.py`
+* publish
+
---
+++ setup.py
@@ -0,0 +1,57 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# setup.py
+# Copyright (C) 2018 Fracpete (fracpete at waikato dot ac dot nz)
+
+from setuptools import setup, find_namespace_packages
+
+
+def _read(f) -> bytes:
+    """
+    Reads in the content of the file.
+    :param f: the file to read
+    :type f: str
+    :return: the content
+    :rtype: str
+    """
+    return open(f, 'rb').read()
+
+
+setup(
+    name="wai.test",
+    description="Python library for unit tests.",
+    long_description=(
+        _read('DESCRIPTION.rst') + b'\n' +
+        _read('CHANGES.rst')).decode('utf-8'),
+    url="https://github.com/waikato-datamining/wai-test",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Programming Language :: Python :: 3',
+    ],
+    license='MIT',
+    package_dir={
+        '': 'src'
+    },
+    packages=find_namespace_packages(where="src"),
+    namespace_packages=[
+        "wai"
+    ],
+    version="0.0.1",
+    author='Corey Sterling',
+    author_email='coreytsterling@gmail.com',
+    install_requires=[],
+    include_package_data=True
+)
